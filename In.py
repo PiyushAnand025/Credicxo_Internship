2c667b925c9a3e8d8e809c1d52c79626bd9d8b4d
PiyushAnand025 <piyushanand025iitkgp@gmail.com>
2020-02-20 15:55:22 +0530

    comments added

--- In.py
+++ In.py
@@ -1,34 +1,21 @@
 # Artificial Neural Network
 
-# Installing Theano
-# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
-
-# Installing Tensorflow
-# Install Tensorflow from the website: https://www.tensorflow.org/versions/r0.12/get_started/os_setup.html
-
-# Installing Keras
-# pip install --upgrade keras
-
-# Part 1 - Data Preprocessing
-
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from keras.models import model_from_json
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+from prettytable import PrettyTable
 
 # Importing the dataset
 dataset = pd.read_csv('musk_csv.csv')
 dataset.drop('conformation_name',inplace=True,axis=1)
 X = dataset.iloc[:, 2:168].values
 y = dataset.iloc[:, 168].values
-
-
-# Encoding categorical data
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder_X_1 = LabelEncoder()
-X[:, 0] = labelencoder_X_1.fit_transform(X[:, 0])
-
-
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
@@ -40,8 +27,6 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-
-# Part 2 - Now let's make the ANN!
 
 # Importing the Keras libraries and packages
 import keras
@@ -57,19 +42,19 @@ classifier.add(Dense(output_dim = 84, init = 'uniform', activation = 'relu', inp
 # Adding the second hidden layer
 classifier.add(Dense(output_dim = 42, init = 'uniform', activation = 'relu'))
 
-# Adding the output layer
+# Adding the third hidden layer
 classifier.add(Dense(output_dim = 21, init = 'uniform', activation = 'relu'))
 
-# Adding the output layer
+# Adding the fourth hidden layer
 classifier.add(Dense(output_dim = 11, init = 'uniform', activation = 'relu'))
 
-# Adding the output layer
+# Adding the fifth hidden layer
 classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu'))
 
-# Adding the output layer
+# Adding the sixth hidden layer
 classifier.add(Dense(output_dim = 4, init = 'uniform', activation = 'relu'))
 
-# Adding the output layer
+# Adding the seventh hidden layer
 classifier.add(Dense(output_dim = 2, init = 'uniform', activation = 'relu'))
 
 # Adding the output layer
@@ -85,13 +70,12 @@ history=classifier.fit(X_train, y_train, batch_size = 10, nb_epoch = 100,validat
 
 y_pred = classifier.predict(X_test)
 y_pred = (y_pred > 0.5)
-# Predicting the Test set results
 
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 
-
+# Printing the loss curve for training and validation dataset
 print(history.history.keys())
 loss_train = history.history['loss']
 loss_val = history.history['val_loss']
@@ -104,7 +88,7 @@ plt.ylabel('Loss')
 plt.legend()
 plt.show()
 
-
+# Printing the accuracy curve for training and validation dataset
 loss_train = history.history['acc']
 loss_val = history.history['val_acc']
 epochs = range(1,101)
@@ -116,3 +100,44 @@ plt.ylabel('Accuracy')
 plt.legend()
 plt.show()
 
+# Saving weights in h5 model
+model_json = classifier.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+classifier.save_weights("model.h5")
+print("Saved model to disk")
+ 
+ #load json and create model
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("model.h5")
+print("Loaded model from disk")
+ 
+# evaluate loaded model on test data
+loaded_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+score = loaded_model.evaluate(X_test, y_test, verbose=0)
+#print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
+
+
+# accuracy: (tp + tn) / (p + n)
+accuracy = accuracy_score(y_test, y_pred)
+print('Accuracy: %f' % accuracy)
+# precision tp / (tp + fp)
+precision = precision_score(y_test, y_pred)
+print('Precision: %f' % precision)
+# recall: tp / (tp + fn)
+recall = recall_score(y_test,y_pred)
+print('Recall: %f' % recall)
+# f1: 2 tp / (2 tp + fp + fn)
+f1 = f1_score(y_test,y_pred)
+print('F1 score: %f' % f1)
+
+x = PrettyTable()
+x.field_names = ["Accuracy", "Precision", "Recall", "f1-score"]
+
+x.add_row([accuracy, precision, recall,f1])
+print(x)
